42670e6b0438a3cf2dd6a58dc549e759dc75887e
Ahnaf <ahnafx88@gmail.com>
2021-12-15 17:45:48 +0600

    version upgraded

--- version.php
+++ version.php
@@ -17,6 +17,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_customleaderboard';
-$plugin->release = '1.0.0';
-$plugin->version = 2021110200;
+$plugin->release = '1.0.1';
+$plugin->version = 2021121500;
 $plugin->requires = 2010112400;
